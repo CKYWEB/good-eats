6f929ae098803110823092e0481a26ca3fcf3610
CKYWEB <yao.siq@northeastern.edu>
2023-11-05 22:41:18 -0500

    add: button component

---
+++ src/app/components/Button/button.js
@@ -0,0 +1,20 @@
+import styles from "./button.module.scss";
+
+export default function Button(props) {
+    const {loading, className, onClick, type="primary"} = props;
+    const buttonStyle = `btn-${type}`;
+
+    return (
+      <button
+        className={`mb-3 btn ${styles[buttonStyle]} w-100 ${className}`}
+        type="button"
+        onClick={onClick}
+      >
+        <span
+          className={`spinner-border spinner-border-sm me-2 ${loading ? "" : "visually-hidden"}`}
+          role="status" 
+        />
+        <span>Continue</span>
+      </button>
+    );
+}
