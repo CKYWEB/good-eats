e853d7d2d498febc0da426704b98b3f3257d6e4d
CKYWEB <yao.siq@northeastern.edu>
2023-11-20 18:25:42 -0500

    rm: old baseurl

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
-// const baseUrl =
-//   process.env.NODE_ENV === "production"
-//     ? "https://good-eats-eight-api.vercel.app"
-//     : "http://localhost:3001";
 const nextConfig = {
     sassOptions: {
         includePaths: [path.join(__dirname, "styles")],
