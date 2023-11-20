ab88aaa5480964f6aabc2bdf087703ecc049e3a0
CKYWEB <yao.siq@northeastern.edu>
2023-11-20 17:21:33 -0500

    add: cors

--- server/api/app.js
+++ server/api/app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const userRouter = require("./routes/user");
 const {USER_PATH} = require("./routes/config");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(USER_PATH, userRouter);
+app.use(cors({
+    origin: "https://good-eats-eight-api.vercel.app/"
+}));
 
 module.exports = app;
