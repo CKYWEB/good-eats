286744c22da4108336e1c3b7075643ac2b7e1928
CKYWEB <yao.siq@northeastern.edu>
2023-11-20 18:07:58 -0500

    change: directly access baseUrl

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://good-eats-eight-api.vercel.app"
+    : "http://localhost:3001";
 const nextConfig = {
     sassOptions: {
         includePaths: [path.join(__dirname, "styles")],
@@ -8,7 +12,7 @@ const nextConfig = {
         return [
             {
                 source: "/api/:path*",
-                destination: `${process.env.SERVER_URL}/api/:path*` // Proxy to Backend
+                destination: `${baseUrl}/api/:path*` // Proxy to Backend
             }
         ];
     }
