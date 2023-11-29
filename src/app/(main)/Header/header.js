54f2f3690601a48903c8fc288d05a2a881596128
ensenginbaieer <lee.hsua@northeastern.edu>
2023-11-29 16:57:30 -0500

    change: login button

--- src/app/(main)/Header/header.js
+++ src/app/(main)/Header/header.js
@@ -4,11 +4,11 @@ import React from "react";
 import styles from "../Header/header.module.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { useRouter } from "next/navigation";
 
 export const Logo = () => {
-  // TODO: below is just an example, you need to rewrite this component
   return (
-    <div className="col">
+    <div className="col d-flex">
       <div className="logo py-3">
         <img
           className={styles.logo__image}
@@ -20,8 +20,26 @@ export const Logo = () => {
   );
 };
 
-export const Avatar = () => {
-  // TODO: below is just an example, you need to rewrite this component
+export const Avatar = ({ isUserLoggedIn, userName, handleLogout }) => {
+  const router = useRouter();
+  //const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
+  const handleLoginClick = () => {
+    if (isUserLoggedIn) {
+      handleLogout();
+    } else {
+      router.push("/login");
+    }
+  };
+
+  const menuItems = [
+    { label: "My Profile", href: "#" },
+    { label: "Search", href: "#" },
+    { label: "Favorite Recipes", href: "#" },
+    { label: "Add a Recipe", href: "#" },
+    { label: "Help", href: "#" },
+  ];
+
   return (
     <div className="col d-flex justify-content-md-end">
       <div className="d-flex gap-2 con">
@@ -37,381 +55,192 @@ export const Avatar = () => {
             <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
           </svg>
         </div>
-        <div
-          id="loginButton"
-          className="visually-hidden"
-        >
-          <a
-            href="login.html"
-            className={styles.loginLink}
-          >
-            Log In
-          </a>
-        </div>
-        <div
-          id="userAvatar"
-          className="dropdown visually-hidden"
-        >
+        {isUserLoggedIn ? (
           <div
-            className={styles.nameUser}
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
+            id="userAvatar"
+            className="dropdown"
           >
-            Unknown
-          </div>
-          <ul
-            id="userDropdown"
-            className="dropdown-menu"
-          >
-            <li>
-              <a
-                className="dropdown-item"
-                href="#"
-              >
-                My Profile
-              </a>
-            </li>
-            <li>
-              <a
-                className="dropdown-item"
-                href="#"
-              >
-                Search
-              </a>
-            </li>
-            <li>
-              <a
-                className="dropdown-item"
-                href="#"
-              >
-                Favorite Recipes
-                <span
-                  id="favorite-count"
-                  className="badge text-bg-secondary"
-                >
-                  0
-                </span>
-              </a>
-            </li>
-            <li>
-              <a
-                className="dropdown-item"
-                href="#"
-              >
-                Add a Recipe
-              </a>
-            </li>
-            <span className="mx-2">
-              ____________________
-            </span>
-            <li>
-              <a
-                className="dropdown-item"
-                href="#"
-              >
-                Help
-              </a>
-            </li>
-            <li
-              data-bs-toggle="modal"
-              data-bs-target="#confirmLogoutModal"
+            <div
+              className={styles.nameUser}
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
             >
-              <a
-                className="dropdown-item"
-                href="#"
+              {userName}
+            </div>
+            <ul
+              id="userDropdown"
+              className="dropdown-menu"
+            >
+              {menuItems.map((item, index) => (
+                <li key={index}>
+                  <a
+                    className="dropdown-item"
+                    href={item.href}
+                  >
+                    {item.label}
+                    {item.label === "Favorite Recipes" && (
+                      <span
+                        id="favorite-count"
+                        className="badge text-bg-secondary"
+                      >
+                        0
+                      </span>
+                    )}
+                  </a>
+                </li>
+              ))}
+              <span className="mx-2">
+                ____________________
+              </span>
+              <li
+                data-bs-toggle="modal"
+                data-bs-target="#confirmLogoutModal"
               >
-                Logout
-              </a>
-            </li>
-          </ul>
-        </div>
+                <button
+                  className="dropdown-item"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </button>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <div id="loginButton">
+            <button
+              onClick={handleLoginClick}
+              className={styles.loginLink}
+            >
+              {isUserLoggedIn ? "Log Out" : "Log In"}
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
-export const ConfirmLogoutModal = () => (
-  <div
-    className="modal fade"
-    id="confirmLogoutModal"
-    tabIndex="-1"
-    aria-labelledby="confirmLogoutModalLabel"
-    aria-hidden="true"
-  >
-    <div className="modal-dialog">
-      <div className="modal-content">
-        <div className="modal-header">
-          <h1
-            className="modal-title fs-5"
-            id="confirmLogoutModalLabel"
-          >
-            Warning
-          </h1>
-          <button
-            type="button"
-            className="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          >
-          </button>
-        </div>
-        <div className="modal-body">
-          Are you sure you want to log out?
-        </div>
-        <div className="modal-footer">
-          <button
-            type="button"
-            className="btn btn-secondary"
-            data-bs-dismiss="modal"
-          >
-            No
-          </button>
-          <button
-            id="logoutButton"
-            type="button"
-            className="btn btn-primary"
-          >
-            Yes
-          </button>
+export const HeaderMenu = () => {
+  const menuItems = [
+    { id: "homepage", label: "Home" },
+    { id: "mealspage", label: "Meals" },
+    { id: "cuisinespage", label: "Cuisines" },
+    { id: "ingredientspage", label: "Ingredients" },
+    { id: "tipsage", label: "Kitchen Tips" },
+    { id: "newspage", label: "News" },
+    { id: "featurespage", label: "Features" },
+    { id: "aboutuspage", label: "About Us" },
+  ];
+
+  return (
+    <nav
+      className={`${styles.navbarHome} navbar navbar-expand-lg border-bottom border-body`}
+      data-bs-theme="light"
+    >
+      <div className="container-fluid">
+        <button
+          className={`${styles.navbarToggler} navbar-toggler`}
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNav"
+          aria-controls="offcanvasNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className="collapse navbar-collapse container-fluid"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            {menuItems.map((item) => (
+              <li
+                key={item.id}
+                className="nav-item active"
+              >
+                <a
+                  id={item.id}
+                  className={`${styles.navLink} nav-link`}
+                  href="#"
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
-    </div>
-  </div>
-);
+    </nav>
+  );
+};
 
-export const HeaderMenu = () => (
-  <nav
-    className={`${styles.navbarHome} navbar navbar-expand-lg border-bottom border-body`}
-    data-bs-theme="light"
-  >
-    <div className="container-fluid">
-      <button
-        className={`${styles.navbarToggler} navbar-toggler`}
-        type="button"
-        data-bs-toggle="offcanvas"
-        data-bs-target="#offcanvasNav"
-        aria-controls="offcanvasNav"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+export const OffcanvasMenu = () => {
+  const menuItems = [
+    { id: "homepage", label: "Home" },
+    { id: "mealspage", label: "Meals" },
+    { id: "cuisinespage", label: "Cuisines" },
+    { id: "ingredientspage", label: "Ingredients" },
+    { id: "tipsage", label: "Kitchen Tips" },
+    { id: "newspage", label: "News" },
+    { id: "featurespage", label: "Features" },
+    { id: "aboutuspage", label: "About Us" },
+  ];
 
-      <div
-        className="collapse navbar-collapse"
-        id="navbarNav"
-      >
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <a
-              id="homepage"
-              className={`${styles.navLink} nav-link`}
-              href="#"
+  return (
+    <div
+      className="offcanvas offcanvas-start"
+      tabIndex="-1"
+      id="offcanvasNav"
+      aria-labelledby="offcanvasNavLabel"
+    >
+      <div className="offcanvas-header">
+        <button
+          type="button"
+          className="btn-close text-reset"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"
+        >
+        </button>
+      </div>
+      <div className="offcanvas-body">
+        <ul className={`${styles.listGroup} list-group`}>
+          {menuItems.map((item) => (
+            <li
+              key={item.id}
+              className={`${styles.listGroupItem} list-group-item list-group-item-action active`}
+              aria-current="true"
             >
-              Home
-            </a>
-          </li>
-          <li className="nav-item active">
-            <a
-              id="mealspage"
-              className={`${styles.navLink} nav-link`}
-              href="#"
-            >
-              Meals
-            </a>
-          </li>
-          <li className="nav-item active">
-            <a
-              id="cuisinespage"
-              className={`${styles.navLink} nav-link`}
-              href="#"
-            >
-              Cuisines
-            </a>
-          </li>
-          <li className="nav-item active">
-            <a
-              id="ingredientspage"
-              className={`${styles.navLink} nav-link`}
-              href="#"
-            >
-              Ingredients
-            </a>
-          </li>
-          <li className="nav-item active">
-            <a
-              id="tipsage"
-              className={`${styles.navLink} nav-link`}
-              href="#"
-            >
-              Kitche Tips
-            </a>
-          </li>
-          <li className="nav-item active">
-            <a
-              id="newspage"
-              className={`${styles.navLink} nav-link`}
-              href="#"
-            >
-              News
-            </a>
-          </li>
-          <li className="nav-item active">
-            <a
-              id="featurespage"
-              className={`${styles.navLink} nav-link`}
-              href="#"
-            >
-              Features
-            </a>
-          </li>
-          <li className="nav-item active">
-            <a
-              id="aboutuspage"
-              className={`${styles.navLink} nav-link`}
-              href="#"
-            >
-              About Us
-            </a>
-          </li>
+              <a
+                id={item.id}
+                className={`${styles.navLink} nav-link`}
+                href="#"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
-  </nav>
-);
+  );
+};
 
-export const OffcanvasMenu = () => (
-  <div
-    className="offcanvas offcanvas-start"
-    tabIndex="-1"
-    id="offcanvasNav"
-    aria-labelledby="offcanvasNavLabel"
-  >
-    <div className="offcanvas-header">
-      <button
-        type="button"
-        className="btn-close text-reset"
-        data-bs-dismiss="offcanvas"
-        aria-label="Close"
-      >
-      </button>
-    </div>
-    <div className="offcanvas-body">
-      <ul className={`${styles.listGroup} list-group`}>
-        <li
-          className={`${styles.listGroupItem} list-group-item list-group-item-action active`}
-          aria-current="true"
-        >
-          <a
-            id="homepage"
-            className={`${styles.navLink} nav-link`}
-            href="#"
-          >
-            Home
-          </a>
-        </li>
-        <li
-          className={`${styles.listGroupItem} list-group-item list-group-item-action active`}
-          aria-current="true"
-        >
-          <a
-            id="mealspage"
-            className={`${styles.navLink} nav-link`}
-            href="#"
-          >
-            Meals
-          </a>
-        </li>
-        <li
-          className={`${styles.listGroupItem} list-group-item list-group-item-action active`}
-          aria-current="true"
-        >
-          <a
-            id="cuisinespage"
-            className={`${styles.navLink} nav-link`}
-            href="#"
-          >
-            Cuisines
-          </a>
-        </li>
-        <li
-          className={`${styles.listGroupItem} list-group-item list-group-item-action active`}
-          aria-current="true"
-        >
-          <a
-            id="ingredientspage"
-            className={`${styles.navLink} nav-link`}
-            href="#"
-          >
-            Ingredients
-          </a>
-        </li>
-        <li
-          className={`${styles.listGroupItem} list-group-item list-group-item-action active`}
-          aria-current="true"
-        >
-          <a
-            id="tipsage"
-            className={`${styles.navLink} nav-link`}
-            href="#"
-          >
-            Kitche Tips
-          </a>
-        </li>
-        <li
-          className={`${styles.listGroupItem} list-group-item list-group-item-action active`}
-          aria-current="true"
-        >
-          <a
-            id="newspage"
-            className={`${styles.navLink} nav-link`}
-            href="#"
-          >
-            News
-          </a>
-        </li>
-        <li
-          className={`${styles.listGroupItem} list-group-item list-group-item-action active`}
-          aria-current="true"
-        >
-          <a
-            id="featurespage"
-            className={`${styles.navLink} nav-link`}
-            href="#"
-          >
-            Features
-          </a>
-        </li>
-        <li
-          className={`${styles.listGroupItem} list-group-item list-group-item-action active`}
-          aria-current="true"
-        >
-          <a
-            id="aboutuspage"
-            className={`${styles.navLink} nav-link`}
-            href="#"
-          >
-            About Us
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-);
 
 export const Header = () => {
   return (
-    // TODO: below is just an example, you need to rewrite this component
     <>
       <header className="bg-light">
-        <div className="container-lg py-3">
+        <div>
           <div className="row row-cols-1 row-cols-md-2">
             <Logo />
             <Avatar />
+          </div>
+          <div className="row">
             <HeaderMenu />
             <OffcanvasMenu />
-            <ConfirmLogoutModal />
           </div>
         </div>
-      </header>
+      </header >
     </>
   );
 };
