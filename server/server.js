39e4b397f0c6cd9d484e2574c672a0b547a1b5fa
CKYWEB <yao.siq@northeastern.edu>
2023-12-01 18:31:05 -0500

    polish: scripts

--- server/server.js
+++ server/server.js
@@ -2,5 +2,5 @@ const PORT = process.env.PORT || 3001;
 const app = require("./api/app");
 
 app.listen(PORT, () => {
-    console.log(`App listening on port: ${PORT}`);
+    console.log(`Server App listening on port: ${PORT}`);
 });
