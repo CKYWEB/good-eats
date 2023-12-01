82049453e3fce60397f85c384a2c19eb8d26f1a1
CKYWEB <yao.siq@northeastern.edu>
2023-12-01 15:56:17 -0500

    add: user store in zustand

---
+++ src/store/user.js
@@ -0,0 +1,12 @@
+import { fetchUserInfo } from "@/api/user";
+import { create } from "zustand";
+
+export const useUserStore = create((set) => ({
+    currentUser: {},
+    fetchCurrentUser: async () => {
+        const res = await fetchUserInfo();
+        set({ 
+            currentUser: res.data,
+        });
+    },
+}));
