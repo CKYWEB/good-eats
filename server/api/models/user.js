26fde2bd12af4c55d46f8d2a0a05b97d286107c5
CKYWEB <yao.siq@northeastern.edu>
2023-12-01 15:19:38 -0500

    add: default role in user model

--- server/api/models/user.js
+++ server/api/models/user.js
@@ -7,7 +7,7 @@ const User = mongoose.model("User", new mongoose.Schema(
         createdDate: { type: Date, default: Date.now()},
         firstName: String,
         lastName: String,
-        role: Number, // 1: admin 2: user
+        role: { type: Number, default: 2 }, // 1: admin 2: user
     },
 ));
 
