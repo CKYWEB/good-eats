836f8869932e18498ec85a4da791ac8c65378ad3
CKYWEB <yao.siq@northeastern.edu>
2023-12-01 14:34:18 -0500

    add: unprotected path list

--- server/api/middleware.js
+++ server/api/middleware.js
@@ -1,13 +1,22 @@
 const jwt = require("jsonwebtoken");
 const {USER_PATH} = require("./routes/config");
+const UNPROTECTED_PATH = [
+    `${USER_PATH}/login`,
+    `${USER_PATH}/create`,
+];
+
+const isPathUnprotected = (path) => {
+    return UNPROTECTED_PATH.includes(path);
+};
 
 const authMiddleware = (req, res, next) => {
-    if (req.path === `${USER_PATH}/login`) {
+    if (isPathUnprotected(req.path)) {
         next();
 
         return;
     }
 
+    // protected paths need to be authorized by token
     const authorizationHeader = req.get("Authorization");
 
     if (!authorizationHeader || !authorizationHeader.startsWith("Bearer ")) {
