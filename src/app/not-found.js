71ba4b7af4dbc197da1b425ad73eef014ea50699
Bubashvilim <123330433+Bubashvilim@users.noreply.github.com>
2023-12-05 13:34:31 -0500

    FIxed transparency. Fixed responsivness.

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,24 +1,35 @@
-import Image from "next/image";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 import "./components/Errors/not-found.scss";
+
  
 export default function NotFound() {
   return (
-    <div className="error-container">
-      <Image
-        className="error-image"
-        src="/images/empty-meal.png"
-        alt="Error Image"
-        width={500}
-        height={500}
-      />
-      <div className="error-message">
-        <h1>
-          404 - Not Found
-        </h1>
-        <p>
-          Sorry, the page you are looking for cannot be found.
-        </p>
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <div className="error-with-image">
+            <Image
+              src="/images/empty-meal.png"
+              alt="Error Image"
+              fluid
+              className="error-image"
+            />
+            <div className="error-message">
+              <h1>
+                404 - Not Found
+              </h1>
+              <p>
+                Sorry, the page you are looking for cannot be found.
+              </p>
+            </div>
+          </div>
+        </Col>
+      </Row>
+     
+      
+    </Container>
   );
 }
