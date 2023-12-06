2fff4c95e8b6c3c75733d19972c7c7ec098bb131
Bubashvilim <123330433+Bubashvilim@users.noreply.github.com>
2023-12-06 16:00:53 -0500

    More fixes to not found error component

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,35 +1,48 @@
+
+
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
+
 import "./components/Errors/not-found.scss";
 
  
 export default function NotFound() {
   return (
     <Container>
-      <Row>
-        <Col>
-          <div className="error-with-image">
-            <Image
-              src="/images/empty-meal.png"
-              alt="Error Image"
-              fluid
-              className="error-image"
-            />
-            <div className="error-message">
-              <h1>
-                404 - Not Found
-              </h1>
-              <p>
-                Sorry, the page you are looking for cannot be found.
-              </p>
-            </div>
-          </div>
+      <Row className="d-flex justify-content-center align-items-center flex-column flex-md-row vh-100">
+        <Col
+          md={4}
+          className="d-flex align-items-center justify-content-center justify-content-md-end mv-250"
+        >
+          <Image
+            src="/images/empty-meal.png"
+            alt="Error Image"
+            fluid
+            width="250px"
+          />
+        </Col>
+        <Col
+          className="error-message col-8 col-md-4"
+        >
+          <h1>
+            Ooops!
+          </h1>
+          <p>
+            Sorry, the page you are looking for cannot be found. Use this 
+            {" "}
+            <a
+              className="text-decoration-underline text-primary"
+              href="/"
+            >
+              link
+            </a>
+            {" "}
+            to go to homepage.
+          </p>
         </Col>
       </Row>
-     
-      
     </Container>
   );
 }
