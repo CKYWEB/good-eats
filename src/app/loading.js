18376d50fda9e2636e43be1da222b60c8897a20f
Bubashvilim <123330433+Bubashvilim@users.noreply.github.com>
2023-12-06 17:05:15 -0500

    Added loading page

---
+++ src/app/loading.js
@@ -0,0 +1,30 @@
+
+"use client";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+
+export default function Loading() {
+  return (
+    <Container>
+      <Row className="d-flex justify-content-center align-items-center flex-column vh-100">
+        <Col className="col-10">
+          <Image
+            src="/images/loader.gif"
+            alt="Loader animation"
+            fluid
+          />
+        </Col>
+        <Col
+          className="col-4"
+        >
+          <h4 className="text-center">
+            Loading...
+          </h4>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
